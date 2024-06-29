43f9d4e8d8b4f546f867fe2d47659191cc6f541c
UrielAC1 <achur@ier.unam.mx>
2024-06-28 19:13:49 -0600

    Own file public
    Doggie Vol.2

---
+++ Perro.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.write("Page dedicated to Frida")
+
+ruta_imagen = 'C:\\Users\\urido\\Pictures\\iPhone_3\\Frida2.jpg'
+
+st.image(ruta_imagen,caption="Frida")
+
+st.write("By Uriel")
+
+
+
+
