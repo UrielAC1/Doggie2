7538ee2598a709998a5b50ba588c78d7d6d1e0f1
UrielAC1 <achur@ier.unam.mx>
2024-06-28 19:26:28 -0600

    Update Perro.py
    Internet image with URL

--- Perro.py
+++ Perro.py
@@ -3,11 +3,12 @@ import streamlit as st
 
 st.write("Page dedicated to Frida")
 
-ruta_imagen = 'C:\\Users\\urido\\Pictures\\iPhone_3\\Frida2.jpg'
+ruta_imagen = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fes%2Ffotos%2Fperro-perfil&psig=AOvVaw2mLQyQ7glPIfILKXROYIKu&ust=1719710632661000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDEqvDT_4YDFQAAAAAdAAAAABAE'
 
 st.image(ruta_imagen,caption="Frida")
 
 st.write("By Uriel")
+
 
 
 
