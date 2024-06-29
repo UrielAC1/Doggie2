1e141964cadef932b3db1aa5b46119626627a0a7
UrielAC1 <achur@ier.unam.mx>
2024-06-28 19:27:25 -0600

    Update Perro.py
    Other URL change

--- Perro.py
+++ Perro.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.write("Page dedicated to Frida")
 
-ruta_imagen = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fes%2Ffotos%2Fperro-perfil&psig=AOvVaw2mLQyQ7glPIfILKXROYIKu&ust=1719710632661000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDEqvDT_4YDFQAAAAAdAAAAABAE'
+ruta_imagen = 'https://media.istockphoto.com/id/639255368/es/vector/vector-silueta-negra-de-un-perro.jpg?s=612x612&w=0&k=20&c=iLRvaN0oXeK5ZefdCtkLN62TCIOodNUZUlP5-h2UFXw='
 
 st.image(ruta_imagen,caption="Frida")
 
